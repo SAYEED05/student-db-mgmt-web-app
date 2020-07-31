7d20ff93e55aeb7c96d19041f709753069975d18
SAYEED <sayeed.705@gmail.com>
2020-07-31 11:30:11 +0530

    implementing collapse nav function

--- app.js
+++ app.js
@@ -7,12 +7,11 @@ var LocalStrategy = require("passport-local");
 var methodOverride = require("method-override");
 var User = require("./models/user");
 
-	 mongoose.connect("mongodb://localhost/studentdb", {
+mongoose.connect("mongodb://localhost/studentdb", {
 	useUnifiedTopology: true,
 	useNewUrlParser: true,
 	useFindAndModify: true,
-}); 
-
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
